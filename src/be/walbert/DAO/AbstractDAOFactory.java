88f79d8f87865badf2eb698a281b955045bf3914
Valbabaw17 <valerio.walbert@condorcet.be>
2022-10-24 20:41:16 +0200

    [DAO]Add 3 files DAO (DAO,DAOFactory,AbstractDAOFactory) without methods

---
+++ src/be/walbert/DAO/AbstractDAOFactory.java
@@ -0,0 +1,31 @@
+package be.walbert.DAO;
+
+
+public abstract class AbstractDAOFactory {
+	public static final int DAO_FACTORY = 0;
+	public static final int XML_DAO_FACTORY = 1;
+
+//	public abstract DAO<Administrator> getAdministratorDAO();
+//	
+//	public abstract DAO<Booking> getBookingDAO();
+//		
+//	public abstract DAO<Copy> getCopyDAO();
+	
+//	public abstract DAO<Loan> getLoanDAO();
+	
+//	public abstract DAO<Player> getPlayerDAO();
+	
+//	public abstract DAO<User> getUserDAO();
+	
+//	public abstract DAO<VideoGames> getVideoGamesDAO();
+	
+	
+	public static AbstractDAOFactory getFactory(int type){
+		switch(type){
+		case DAO_FACTORY:
+			return new DAOFactory();
+			default:
+				return null;
+		}
+	}
+}
---
+++ src/be/walbert/DAO/DAOFactory.java
@@ -0,0 +1,34 @@
+package be.walbert.DAO;
+
+import java.sql.Connection;
+
+public class DAOFactory extends AbstractDAOFactory{
+	protected static final Connection conn = be.walbert.connection.ProjectConnection.getInstance();
+	
+//	public DAO<Administrator> getAdministratorDAO(){
+//		return new AdministratorDAO(conn);
+//	}
+//	
+//	public DAO<Booking> getBookingDAO(){
+//		return new BookingDAO(conn);
+//	}
+//	
+//	public DAO<Copy> getCopyDAO(){
+//		return new CopyDAO(conn);
+//	}
+	
+//	public DAO<Loan> getLoanDAO(){
+//	return new LoanDAO(conn);
+//}
+//	public DAO<Player> getPlayerDAO(){
+//	return new PlayerDAO(conn);
+//}
+//	public DAO<User> getUserDAO(){
+//		return new UserDAO(conn);
+//	}
+//		
+//	public DAO<VideoGame> getVideoGameDAO(){
+//	return new VideoGameDAO(conn);
+//}
+	
+}
