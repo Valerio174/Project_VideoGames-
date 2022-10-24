7ebc0829a7671b45d255299eb30d9e551f6bc062
Valbabaw17 <valerio.walbert@condorcet.be>
2022-10-24 20:26:44 +0200

    Creation of ProjectConnection(access to database)

---
+++ src/be/walbert/connection/ProjectConnection.java
@@ -0,0 +1,38 @@
+package be.walbert.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.swing.JOptionPane;
+
+public class ProjectConnection {
+	private static Connection instance = null;
+	
+	private ProjectConnection(){
+		try{
+			Class.forName("net.ucanaccess.jdbc.UcanaccessDriver");
+			String url = "jdbc:ucanaccess://./VideoGames.accdb";
+			instance = DriverManager.getConnection(url);
+		}
+		catch(ClassNotFoundException ex){
+			JOptionPane.showMessageDialog(null, "Classe de driver introuvable" + ex.getMessage());
+			System.exit(0);
+		}
+		catch (SQLException ex) {
+			JOptionPane.showMessageDialog(null, "Erreur JDBC : " + ex.getMessage());
+		}
+		if (instance == null) {
+            JOptionPane.showMessageDialog(null, "La base de données est inaccessible, fermeture du programme.");
+            System.exit(0);
+        }
+	}
+	
+	public static Connection getInstance() {
+		if(instance == null){
+			new ProjectConnection();
+		}
+		return instance;
+	}
+
+}
