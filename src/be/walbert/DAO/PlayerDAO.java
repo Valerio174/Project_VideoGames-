5494b10e2ed5bd57dafb5e0098c2ebfcf6b820dc
Valbabaw17 <valerio.walbert@condorcet.be>
2022-10-27 21:43:24 +0200

    [DAO]Implement create method to add also in Player table (get the last id_users added)

--- src/be/walbert/DAO/PlayerDAO.java
+++ src/be/walbert/DAO/PlayerDAO.java
@@ -28,11 +28,24 @@ public class PlayerDAO extends DAO<Player>{
 			ps.setString(1, newplayer.getUsername());
 			ps.setString(2, newplayer.getPassword());
 			ps.setString(3, "Player");
-			
 			ps.execute();
 			
+			ResultSet rs = ps.getGeneratedKeys();
+			int generatedKey = 0;
+			if (rs.next()) {
+			    generatedKey = rs.getInt(1);
+			}
+			
+			PreparedStatement ps2 = connect.prepareStatement("INSERT INTO Player(id_users,credit, pseudo,registrationDate,dateOfBirth) VALUES(?,?,?,?,?)");
+			ps2.setInt(1, generatedKey);
+			ps2.setInt(2, 10);
+			ps2.setString(3, newplayer.getPseudo());
+			ps2.setDate(4, Date.valueOf(newplayer.getRegistrationDate()));
+			ps2.setDate(5, Date.valueOf(newplayer.getDateOfBirth()));
+			ps2.executeUpdate();
 			
 			ps.close();
+			ps2.close();
 			return true;
 		}
 		catch(SQLException e){
