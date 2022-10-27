bd492a5195dd11957dd66b658610bac512f60ce7
Valbabaw17 <valerio.walbert@condorcet.be>
2022-10-27 21:41:36 +0200

    [DAO]Implement create method to add Player in Users table

--- src/be/walbert/DAO/PlayerDAO.java
+++ src/be/walbert/DAO/PlayerDAO.java
@@ -1,42 +1,53 @@
 package be.walbert.DAO;
 
 import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
-import be.walbert.classes.Administrator;
 import be.walbert.classes.Player;
-import be.walbert.classes.User;
 
 public class PlayerDAO extends DAO<Player>{
-	
 	public PlayerDAO(Connection conn) {
 		super(conn);
-		// TODO Auto-generated constructor stub
 	}
 
 	/*Récupérer Player correspondant */
 	@Override
 	public Player find(Player newplayer) {
-		 
 		return newplayer;
 	}
 
 	@Override
-	public boolean create(Player obj) {
-		// TODO Auto-generated method stub
+	public boolean create(Player newplayer) {
+		try{
+			//connect.close();
+			PreparedStatement ps = connect.prepareStatement("INSERT INTO Users(username, password,type) VALUES(?,?,?)",Statement.RETURN_GENERATED_KEYS);
+			ps.setString(1, newplayer.getUsername());
+			ps.setString(2, newplayer.getPassword());
+			ps.setString(3, "Player");
+			
+			ps.execute();
+			
+			
+			ps.close();
+			return true;
+		}
+		catch(SQLException e){
+			e.printStackTrace();
+		}
 		return false;
 	}
 
 	@Override
-	public boolean delete(Player obj) {
-		// TODO Auto-generated method stub
+	public boolean delete(Player obj) { 
 		return false;
 	}
 
 	@Override
-	public boolean update(Player obj) {
-		// TODO Auto-generated method stub
+	public boolean update(Player obj) { 
 		return false;
 	}
 }
