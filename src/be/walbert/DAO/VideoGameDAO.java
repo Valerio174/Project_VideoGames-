27321a436eb7e40fccddd1cd9d86a66cb41ec3b5
Valbabaw17 <valerio.walbert@condorcet.be>
2022-10-30 11:21:54 +0100

    [DAO]Implement findAll method(return ArrayList<VideoGame> to have all videogames in database

--- src/be/walbert/DAO/VideoGameDAO.java
+++ src/be/walbert/DAO/VideoGameDAO.java
@@ -1,6 +1,10 @@
 package be.walbert.DAO;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
 import be.walbert.classes.VideoGame;
 
 public class VideoGameDAO extends DAO<VideoGame>{
@@ -9,6 +13,26 @@ public class VideoGameDAO extends DAO<VideoGame>{
 		super(conn); 
 	}
 
+	public ArrayList<VideoGame> findAll(){
+		ArrayList<VideoGame> all_videogames = new ArrayList<>();
+		
+		try {
+			ResultSet result = this.connect.createStatement(
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY).executeQuery("SELECT vg.id_VideoGame, vg.name, vg.creditCost, v.name_version, c.name_console\r\n"
+							+ "FROM ( VideoGame vg LEFT OUTER JOIN Version v ON vg.id_version = v.id_version ) LEFT OUTER JOIN Console c ON v.id_console= c.id_console");
+			while(result.next()){
+				VideoGame newvideogame = new VideoGame(result.getInt("id_VideoGame"),result.getString("name"),result.getInt("creditCost"),result.getString("name_version"),result.getString("name_console"));
+				all_videogames.add(newvideogame);
+			}
+		} 
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return all_videogames;
+		
+	}
 	@Override
 	public boolean create(VideoGame obj) { 
 		return false;
