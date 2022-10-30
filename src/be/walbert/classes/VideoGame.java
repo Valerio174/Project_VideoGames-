aee70a1c8abb7d0ff0da9ba99ba92237dc2ae5ca
Valbabaw17 <valerio.walbert@condorcet.be>
2022-10-30 10:39:17 +0100

    [classes.VideoGame]Modify VideoGame (attributs+constructor+properties)

--- src/be/walbert/classes/VideoGame.java
+++ src/be/walbert/classes/VideoGame.java
@@ -6,11 +6,56 @@ import java.util.ArrayList;
 public class VideoGame{
 
 	/*Attributs*/
+	private int id_videogame;
+	private String name;
+	private int creditCost;
+	private String console;
+	private String version;
 	private ArrayList<Copy> copy_list;
 	private ArrayList<Booking> booking_list;
 
 	
 	/*Getters/Setters*/
+	public int getId_videogame() {
+		return id_videogame;
+	}
+
+	public void setId_videogame(int id_videogame) {
+		this.id_videogame = id_videogame;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getCreditCost() {
+		return creditCost;
+	}
+
+	public void setCreditCost(int creditCost) {
+		this.creditCost = creditCost;
+	}
+
+	public String getConsole() {
+		return console;
+	}
+
+	public void setConsole(String console) {
+		this.console = console;
+	}
+
+	public String getVersion() {
+		return version;
+	}
+
+	public void setVersion(String version) {
+		this.version = version;
+	}
+
 	public ArrayList<Copy> getList_copy() {
 		return copy_list;
 	}
@@ -28,7 +73,12 @@ public class VideoGame{
 	}
 
 	/*Constructeurs*/
-	public VideoGame() {
+	public VideoGame( int id_videogame, String name, int creditCost, String console, String version) {
+		this.id_videogame = id_videogame;
+		this.name=name;
+		this.creditCost=creditCost;
+		this.console=console;
+		this.version=version;
 		this.copy_list = new ArrayList<>();
 		this.booking_list = new ArrayList<>();
 	}
