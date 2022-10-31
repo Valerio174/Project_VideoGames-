e499178a0a81fb7ccdd94835ae083aa7064b2e9d
Valbabaw17 <valerio.walbert@condorcet.be>
2022-10-31 16:17:13 +0100

    [frames]Add JTable to print all videogames+listener to click on a game + redirect to new GamePage

--- src/be/walbert/frames/CatalogVideoGames.java
+++ src/be/walbert/frames/CatalogVideoGames.java
@@ -2,22 +2,44 @@ package be.walbert.frames;
 
 import java.awt.EventQueue;
 
+import javax.swing.DefaultListModel;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 import be.walbert.classes.User;
+import be.walbert.classes.VideoGame;
+
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.util.ArrayList;
+import java.util.Iterator;
+
 import javax.swing.JTextArea;
 import javax.swing.JTextPane;
+import javax.swing.ListCellRenderer;
+import javax.swing.ListModel;
+import javax.swing.ListSelectionModel;
 import javax.swing.ScrollPaneConstants;
 import javax.swing.JScrollBar;
+import javax.swing.JList;
+import javax.swing.JOptionPane;
+
+import java.awt.Color;
+import javax.swing.border.MatteBorder;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.JTable;
+import java.awt.List;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.table.DefaultTableModel;
 
 public class CatalogVideoGames extends JFrame {
 
-	private JPanel contentPane;
-
+	private JPanel contentPane;  
+	private ArrayList<VideoGame> videogames;
+	private JTable table;
 	/**
 	 * Launch the application.
 	 */
@@ -39,7 +61,7 @@ public class CatalogVideoGames extends JFrame {
 	 */
 	public CatalogVideoGames(User user) {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(0, 0, 998, 582); 
+		setBounds(0, 0, 1169, 765); 
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
@@ -48,9 +70,59 @@ public class CatalogVideoGames extends JFrame {
 	 
 		JLabel lbl_Username = new JLabel("");
 		lbl_Username.setFont(new Font("Tahoma", Font.PLAIN, 25));
-		lbl_Username.setBounds(47, 33, 1162, 49);
+		lbl_Username.setBounds(47, 33, 1074, 49);
 		contentPane.add(lbl_Username);
 		lbl_Username.setText("Welcome on the catalog of VideoGames " + user.getUsername());
-		 
+		
+		/*Label de test*/
+		JLabel lbl_TEST = new JLabel("");
+		lbl_TEST.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lbl_TEST.setBounds(96, 663, 252, 54);
+		contentPane.add(lbl_TEST);
+		
+		
+		/*Liste des jeux videos*/
+		videogames = VideoGame.getAll();
+		
+			/*Noms des colonnes du tableau*/
+		String[] columnsnames = {"Name", "Console","Version","Credits"};
+        
+			/*Model du tableau*/
+		DefaultTableModel tablemodel = new DefaultTableModel() {
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		};
+			/*Attribuer noms des colonnes au tablemodel*/
+		tablemodel.setColumnIdentifiers(columnsnames);	
+	       for(VideoGame v: videogames){
+	           Object[] data = {v.getId_videogame(), v.getName(), v.getConsole(),v.getVersion(),v.getCreditCost()};
+	           tablemodel.addRow(data);
+	       }
+	        
+	       /*Creation du curseur du JTable*/
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setSize(780, 284);
+        scrollPane.setLocation(47, 180);
+        contentPane.add(scrollPane);
+        
+        	/*Creation du JTable*/
+        table = new JTable(tablemodel);
+        scrollPane.setViewportView(table);
+        
+        /*Ajouter evenement lors du clique sur une ligne*/
+	    table.addMouseListener(new MouseAdapter() {
+	    	public void mouseClicked(MouseEvent e){
+	    	      if (e.getClickCount() == 2){
+	    	    	  int selectedRow = table.getSelectedRow();
+	  					int id_videogame = (int) table.getValueAt(selectedRow, 0);
+	  					GamePage gamepage= new GamePage(id_videogame);
+	  					gamepage.setVisible(true);
+	  					dispose();
+	    	         }
+	    	      }
+	    });
+        
 	}
 }
