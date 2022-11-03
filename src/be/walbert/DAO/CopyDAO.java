462ca4e5fa1b9a80de79aecc31e1145a3792d66f
Valbabaw17 <valerio.walbert@condorcet.be>
2022-11-03 15:19:02 +0100

    [DAO]Create findAllCopy to have all copy of the game passed by parameter

--- src/be/walbert/DAO/CopyDAO.java
+++ src/be/walbert/DAO/CopyDAO.java
@@ -1,8 +1,14 @@
 package be.walbert.DAO;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.ArrayList;
 
 import be.walbert.classes.Copy;
+import be.walbert.classes.Player;
+import be.walbert.classes.VideoGame;
 
 public class CopyDAO extends DAO<Copy> {
 
@@ -28,6 +34,33 @@ public class CopyDAO extends DAO<Copy> {
 	@Override
 	public Copy find(Copy obj) { 
 		return null;
+	}
+	
+	public ArrayList<Copy> findAllCopy(VideoGame videogame){
+		ArrayList<Copy> list_copy = new ArrayList<>();
+		
+		try {
+			ResultSet result = this.connect.createStatement(
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY).executeQuery("SELECT Copy.id_copy, Copy.id_VideoGame, VideoGame.name, VideoGame.creditCost, Version.name_version, Console.name_console, Users.username, Users.password, Player.credit, Player.pseudo, Player.registrationDate, Player.dateOfBirth\r\n"
+							+ "FROM ((((Copy LEFT JOIN VideoGame ON Copy.id_VideoGame = VideoGame.id_VideoGame) "
+							+ "LEFT JOIN Version ON VideoGame.id_version = Version.id_version) "
+							+ "LEFT JOIN Console ON Version.id_console = Console.id_console) "
+							+ "LEFT JOIN Player ON Copy.id_users_lender = Player.id_users) "
+							+ "LEFT JOIN Users ON Player.id_users = Users.id_users WHERE Copy.id_VideoGame = " + videogame.getId_videogame());
+			while(result.next()){
+				Copy newcopy = new Copy(result.getInt("id_copy"), new Player(result.getString("username"),result.getString("password"),
+						result.getInt("credit"), result.getString("pseudo"), result.getDate("registrationDate").toLocalDate(),
+						result.getDate("dateOfBirth").toLocalDate()), videogame);
+				list_copy.add(newcopy);
+			}
+		} 
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return list_copy;
+		
 	}
 
 	
