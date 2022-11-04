5c396d2ff37aef96b0fe4874c80c604ebd557864
Valbabaw17 <valerio.walbert@condorcet.be>
2022-11-04 10:47:57 +0100

    [frames]Add credits of the game + delete the list of copy(not necessary)

--- src/be/walbert/frames/GamePage.java
+++ src/be/walbert/frames/GamePage.java
@@ -5,8 +5,12 @@ import java.awt.EventQueue;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
 
+import be.walbert.classes.Copy;
 import be.walbert.classes.User;
 import be.walbert.classes.VideoGame;
 import javax.swing.JLabel;
@@ -16,11 +20,14 @@ import java.awt.Image;
 import javax.swing.SwingConstants;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.util.ArrayList;
+import javax.swing.JButton;
 
 public class GamePage extends JFrame {
 
 	private JPanel contentPane;
 	private Image img = new ImageIcon(this.getClass().getResource("/ressources/icon_back.png")).getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+	
 	/**
 	 * Launch the application.
 	 */
@@ -75,38 +82,56 @@ public class GamePage extends JFrame {
 		
 		JLabel lbl_TitleName = new JLabel("Name:");
 		lbl_TitleName.setFont(new Font("Segoe UI Black", Font.BOLD, 20));
-		lbl_TitleName.setBounds(21, 159, 106, 32);
+		lbl_TitleName.setBounds(21, 109, 106, 32);
 		contentPane.add(lbl_TitleName);
 		
 		JLabel lbl_TitleConsole = new JLabel("Console:");
 		lbl_TitleConsole.setFont(new Font("Segoe UI Black", Font.BOLD, 20));
-		lbl_TitleConsole.setBounds(21, 224, 106, 32);
+		lbl_TitleConsole.setBounds(21, 169, 106, 32);
 		contentPane.add(lbl_TitleConsole);
 		
 		JLabel lbl_TitleVersion = new JLabel("Version:");
 		lbl_TitleVersion.setFont(new Font("Segoe UI Black", Font.BOLD, 20));
-		lbl_TitleVersion.setBounds(21, 290, 106, 32);
+		lbl_TitleVersion.setBounds(21, 228, 106, 32);
 		contentPane.add(lbl_TitleVersion);
 		
 		JLabel lbl_GameName = new JLabel("");
 		lbl_GameName.setFont(new Font("Segoe UI Emoji", Font.BOLD, 20));
-		lbl_GameName.setBounds(148, 163, 361, 32);
+		lbl_GameName.setBounds(148, 113, 361, 32);
 		contentPane.add(lbl_GameName);
 		
 		JLabel lbl_GameConsole = new JLabel("");
 		lbl_GameConsole.setFont(new Font("Segoe UI Emoji", Font.BOLD, 20));
-		lbl_GameConsole.setBounds(148, 224, 361, 32);
+		lbl_GameConsole.setBounds(148, 173, 361, 32);
 		contentPane.add(lbl_GameConsole);
 		
 		JLabel lbl_GameVersion = new JLabel("");
 		lbl_GameVersion.setFont(new Font("Segoe UI Emoji", Font.BOLD, 20));
-		lbl_GameVersion.setBounds(148, 290, 361, 32);
+		lbl_GameVersion.setBounds(148, 232, 361, 32);
 		contentPane.add(lbl_GameVersion);
 		
+		JButton btnNewButton = new JButton("Loan game");
+		btnNewButton.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		btnNewButton.setBounds(341, 572, 136, 32);
+		contentPane.add(btnNewButton);
+		btnNewButton.setVisible(false);
 		
+		JLabel lbl_TitleCredits = new JLabel("Cost:");
+		lbl_TitleCredits.setFont(new Font("Segoe UI Black", Font.BOLD, 20));
+		lbl_TitleCredits.setBounds(21, 288, 106, 32);
+		contentPane.add(lbl_TitleCredits);
+		
+		JLabel lbl_GameCredits = new JLabel((String) null);
+		lbl_GameCredits.setFont(new Font("Segoe UI Emoji", Font.BOLD, 20));
+		lbl_GameCredits.setBounds(148, 288, 361, 32);
+		contentPane.add(lbl_GameCredits);
+        
 		/*Attribuer les infos du jeux aux différents labels*/
 		lbl_GameName.setText(videogame_selected.getName());
 		lbl_GameConsole.setText(videogame_selected.getConsole());
 		lbl_GameVersion.setText(videogame_selected.getVersion());
+		lbl_GameCredits.setText(videogame_selected.getCreditCost()+ " Credits");
+
+		
 	}
 }
