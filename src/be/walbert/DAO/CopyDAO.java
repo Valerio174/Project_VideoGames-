5957fc78678a8e9b84ff4ab57eb3f4266d14884e
Valbabaw17 <valerio.walbert@condorcet.be>
2022-11-07 18:32:50 +0100

    [DAO]Modify CopyAvailable-> d_users  borrower not the same as lender

--- src/be/walbert/DAO/CopyDAO.java
+++ src/be/walbert/DAO/CopyDAO.java
@@ -62,15 +62,16 @@ public class CopyDAO extends DAO<Copy> {
 		return list_copy;
 	}
 		
-	public ArrayList<Copy> CopyAvailable(VideoGame videogame) {
+	public ArrayList<Copy> CopyAvailable(VideoGame videogame, Player borrower) {
 		ArrayList<Copy> all_copy = new ArrayList<>();
 		
 		try {
 			ResultSet result = this.connect.createStatement(
 					ResultSet.TYPE_SCROLL_INSENSITIVE,
-					ResultSet.CONCUR_READ_ONLY).executeQuery("SELECT Player.pseudo, Player.credit, Player.registrationDate, Player.dateOfBirth, Users.username, Users.password, *\r\n"
-							+ "FROM ((Copy LEFT JOIN Loan ON Copy.id_copy = Loan.id_copy) LEFT JOIN Player ON Copy.id_users_lender = Player.id_users) LEFT JOIN Users ON Player.id_users = Users.id_users\r\n"
-							+ "WHERE (((Loan.id_loan) Is Null) AND ((Copy.id_videogame)="+videogame.getId_videogame()+")) OR (((Copy.id_videogame)="+videogame.getId_videogame()+") AND ((Loan.ongoing)=False))");
+					ResultSet.CONCUR_READ_ONLY).executeQuery("SELECT Copy.id_copy, Player.pseudo, Player.credit, Player.registrationDate, Player.dateOfBirth, Users.username, Users.password\r\n"
+							+ "FROM ((Copy LEFT JOIN Player ON Copy.id_users_lender = Player.id_users) LEFT JOIN Users ON Player.id_users = Users.id_users) LEFT JOIN Loan ON Copy.id_copy = Loan.id_copy\r\n"
+							+ "WHERE ((Not (Users.username)=\""+borrower.getUsername()+"\") AND ((Loan.id_loan) Is Null) AND ((Copy.id_videogame)="+videogame.getId_videogame()+")) OR (((Copy.id_videogame)="+videogame.getId_videogame()+") AND ((Loan.ongoing)=False));\r\n"
+							+ "");
 			while(result.next()){
 				Copy newcopy = new Copy(result.getInt("id_copy"), new Player(result.getString("username"),result.getString("password"),
 						result.getInt("credit"),result.getString("pseudo"),
@@ -85,5 +86,25 @@ public class CopyDAO extends DAO<Copy> {
 		
 		return all_copy;
 
+	}
+
+	public ArrayList<Copy> OwnCopy(Player player_owner){
+		ArrayList<Copy> all_copy = new ArrayList<>();
+		
+		try {
+			ResultSet result = this.connect.createStatement(
+					ResultSet.TYPE_SCROLL_INSENSITIVE,
+					ResultSet.CONCUR_READ_ONLY).executeQuery("SELECT Copy.id_copy, Copy.id_VideoGame, VideoGame.name, VideoGame.creditCost, Version.name_version, Console.name_console, Copy.id_users_lender, Users.username, Users.password, Player.credit, Player.pseudo, Player.registrationDate, Player.dateOfBirth FROM (((Copy LEFT JOIN (Player LEFT JOIN Users ON Player.id_users = Users.id_users) ON Copy.id_users_lender = Player.id_users) LEFT JOIN VideoGame ON Copy.id_VideoGame = VideoGame.id_VideoGame) LEFT JOIN Version ON VideoGame.id_version = Version.id_version) LEFT JOIN Console ON Version.id_console = Console.id_console WHERE (((Users.username)=\""+player_owner.getUsername()+ "\"))");
+			while(result.next()){
+				Copy newcopy = new Copy(result.getInt("id_copy"),player_owner,new VideoGame(result.getInt("id_videogame"), result.getString("name"), result.getInt("creditCost"), result.getString("name_version"), result.getString("name_console")));
+				all_copy.add(newcopy);
+			}
+		} 
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+		
+		return all_copy;
+		
 	}
 }
