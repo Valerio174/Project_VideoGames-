770b39b4da86833aaf6e8735b1041ecca2b02c59
Valbabaw17 <valerio.walbert@condorcet.be>
2022-11-18 12:04:31 +0100

    [frames]Creation of MyLoansPage frame to gat all loans for a Player

---
+++ src/be/walbert/frames/MyLoansPage.java
@@ -0,0 +1,129 @@
+package be.walbert.frames;
+
+import java.awt.EventQueue;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import javax.swing.ImageIcon;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.SwingConstants;
+import javax.swing.border.EmptyBorder;
+import javax.swing.table.DefaultTableModel;
+
+import be.walbert.classes.Copy;
+import be.walbert.classes.Loan;
+import be.walbert.classes.Player;
+
+public class MyLoansPage extends JFrame {
+
+	private JPanel contentPane;
+	private JTable table;
+	private Image icon_logout = new ImageIcon(this.getClass().getResource("/ressources/icon_logout.png")).getImage().getScaledInstance(64, 64, Image.SCALE_SMOOTH);
+	private Image icon_back = new ImageIcon(this.getClass().getResource("/ressources/icon_back.png")).getImage().getScaledInstance(50, 50, Image.SCALE_SMOOTH);
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					HomePage homepage = new HomePage();
+					homepage.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public MyLoansPage(Player player) {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1231, 771);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		
+		/*Icon de retour (vers la page Admin)*/
+		JLabel lbl_icon_back = new JLabel("");
+		
+		/*Ajout de l'evenement lors du click*/
+		lbl_icon_back.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				CatalogVideoGames catalog = new CatalogVideoGames(player);
+				catalog.setVisible(true);
+				dispose();
+			}
+		});
+		lbl_icon_back.setBounds(21, 620, 50, 50); 
+		lbl_icon_back.setIcon(new ImageIcon(icon_back));
+		contentPane.add(lbl_icon_back);
+		
+		/*Bouton deconnexion(Log Out)*/
+		JLabel lbl_icon_logout = new JLabel("");
+		
+		/*Ajout de l'evenement sur le bouton deconnexion*/
+		lbl_icon_logout.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				HomePage homepage = new HomePage();	/*Redirection vers la HomePage*/
+				homepage.setVisible(true);
+				dispose();
+			}
+		});
+		lbl_icon_logout.setBounds(1051, 48, 70, 90); 
+		lbl_icon_logout.setIcon(new ImageIcon(icon_logout));
+		contentPane.add(lbl_icon_logout);
+		
+		JLabel lbl_LogOut = new JLabel("Log Out");
+		lbl_LogOut.setHorizontalAlignment(SwingConstants.CENTER);
+		lbl_LogOut.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lbl_LogOut.setBounds(1022, 11, 99, 50);
+		contentPane.add(lbl_LogOut);
+		
+		/*Noms des colonnes du tableau*/
+		String[] columnsnames = { "Start Date","End Date","Ongoing","Game","Version","Console"};
+        
+			/*Model du tableau*/
+		DefaultTableModel tablemodel = new DefaultTableModel() {
+			@Override
+			public boolean isCellEditable(int row, int column) {
+				return false;
+			}
+		};
+			/*Attribuer noms des colonnes au tablemodel*/
+		tablemodel.setColumnIdentifiers(columnsnames);	
+	       for(Loan l: player.getLender_list()){
+	           Object[] datas = {l.getStartDate(), l.getEndDate(), l.isOngoing(), l.getCopy().getGame().getName(),l.getCopy().getGame().getVersion(), l.getCopy().getGame().getConsole()};
+	           tablemodel.addRow(datas);
+	       }
+	        
+	       /*Creation du curseur du JTable*/
+        contentPane.setLayout(null);
+        JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setSize(543, 233);
+        scrollPane.setLocation(22, 138);
+        contentPane.add(scrollPane);
+        
+        	/*Creation du JTable*/
+        table = new JTable(tablemodel);
+        scrollPane.setViewportView(table);
+        
+        JLabel lbl_Title_ListCopies = new JLabel("The list of your loans:");
+        lbl_Title_ListCopies.setFont(new Font("Segoe UI Semibold", Font.PLAIN, 18));
+        lbl_Title_ListCopies.setBounds(22, 77, 452, 47);
+        contentPane.add(lbl_Title_ListCopies);
+	}
+
+}
