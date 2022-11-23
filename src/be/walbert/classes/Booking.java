29515ebb482d3d5b53ffd918c72a67ea66eb0c43
Valbabaw17 <valerio.walbert@condorcet.be>
2022-11-23 13:27:51 +0100

    [classes.Booking]Add CreateBooking method in Booking class

--- src/be/walbert/classes/Booking.java
+++ src/be/walbert/classes/Booking.java
@@ -1,16 +1,29 @@
-
-
 package be.walbert.classes;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
-public class Booking{
+import be.walbert.DAO.AbstractDAOFactory;
+import be.walbert.DAO.DAO;
 
+public class Booking implements Serializable{
+
+	private static final long serialVersionUID = 4447518906972680337L;
 	/*Attributs*/
+	private int id_booking;
 	private LocalDate bookingDate;
 	private VideoGame game;
+	private Player player;
+	AbstractDAOFactory adf = AbstractDAOFactory.getFactory(AbstractDAOFactory.DAO_FACTORY);
+	DAO<Booking> bookingDAO = adf.getBookingDAO();
 	
 	/*Getters/Setters*/
+	public int getId_booking() {
+		return id_booking;
+	}
+	public void setId_booking(int id_booking) {
+		this.id_booking = id_booking;
+	}
 	public LocalDate getBookingDate() {
 		return bookingDate;
 	}
@@ -24,13 +37,29 @@ public class Booking{
 	public void setGame(VideoGame game) {
 		this.game = game;
 	}
-
+	
+	public Player getPlayer() {
+		return player;
+	}
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
 	/*Constructeurs*/
-	public Booking(LocalDate bookingDate, VideoGame game) {
+	public Booking(LocalDate bookingDate, VideoGame game, Player player) {
 		this.bookingDate = bookingDate;
 		this.game = game;
+		this.player = player;
+	}	
+	public Booking(int id_booking, LocalDate bookingDate, VideoGame game, Player player) {
+		this.id_booking = id_booking;
+		this.bookingDate = bookingDate;
+		this.game = game;
+		this.player = player;
 	}	
 	
+	public boolean CreateBooking() {
+		return bookingDAO.create(this);
+	}
 	/*Méthodes*/
 	public void Delete() {
 		
